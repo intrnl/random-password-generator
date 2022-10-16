659b63ed1dbdc64197692f315f62e4279fc03ca3
intrnl <intrnl28+git@gmail.com>
2022-10-17 06:24:08 +0700

    chore: minify html

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -9,6 +9,8 @@ fi
 GIT_COMMIT=$(git rev-parse HEAD)
 
 rsync -aHAX --delete --exclude=.git dist/ deploy/
+pnpm exec minify-html deploy/index.html --output deploy/index.html
+
 git -C deploy/ add .
 git -C deploy/ commit -m "deploy: ${GIT_COMMIT}"
 git -C deploy/ push
