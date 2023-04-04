9325e520d9df48542f696583f1d77c9b4835e2bf
intrnl <intrnl28+git@gmail.com>
2023-04-04 09:19:17 +0700

    chore: upgrade dependencies

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,103 +1,63 @@
-import * as os from 'os';
-import * as http from 'http';
 import * as esbuild from 'esbuild';
+import * as os from 'os';
 
 import { config } from '../esbuild.config.js';
-
 
 const args = process.argv.slice(2);
 
 const serverOptions = {
-	host: args.includes('--host') ? '0.0.0.0' : '127.0.0.1',
-	port: 3000,
+	host: args.includes('--expose') ? '0.0.0.0' : '127.0.0.1',
+	port: undefined,
 };
 
-const internal = await esbuild.serve({
-	servedir: 'dist/',
-}, {
+const printServerInfo = ({ host, port }) => {
+	if (host === '127.0.0.1') {
+		console.info(`> local: http://${host}:${port}`);
+		console.info(`> network: not exposed`);
+	}
+	else {
+		const mapping = os.networkInterfaces();
+
+		for (const intrf in mapping) {
+			const addresses = mapping[intrf];
+
+			for (const address of addresses) {
+				// for some reason, @types/node tells you that `.family` is a string,
+				// while in reality, it's a number, so we'll just check for both.
+				if (address.family !== 'IPv4' && address.family !== 4) {
+					continue;
+				}
+
+				const type = address.internal ? 'local' : 'network';
+				const hostname = address.internal ? 'localhost' : address.address;
+
+				console.info(`> ${type}: http://${hostname}:${port} (${intrf})`);
+			}
+		}
+	}
+
+	console.log('');
+};
+
+const context = await esbuild.context({
 	minify: false,
 	...config,
 	format: 'esm',
 	bundle: true,
 	splitting: true,
 	sourcemap: true,
+	define: {
+		'DEV': 'true',
+		'COMMIT_HASH': '"DEV"',
+	},
 	plugins: [
 		...config.plugins || [],
 	],
 });
 
-const server = http.createServer((request, response) => {
-	const options = {
-		hostname: internal.host,
-		port: internal.port,
-		path: request.url,
-		method: request.method,
-		header: request.headers,
-	};
-
-	const handleProxyRequest = (res) => {
-		if (res.statusCode === 404 && options.path !== '/') {
-			options.path = '/';
-
-			const proxy = http.request(options, handleProxyRequest);
-			request.pipe(proxy, { end: true });
-			return;
-		}
-
-		response.writeHead(res.statusCode, res.headers);
-		res.pipe(response, { end: true });
-	};
-
-	const proxy = http.request(options, handleProxyRequest);
-	request.pipe(proxy, { end: true });
+const internal = await context.serve({
+	servedir: 'dist/',
+	host: serverOptions.host,
 });
 
-const result = await startServer(server, serverOptions);
-printServerInfo(result);
-
-
-function startServer (server, options) {
-	const { host, port } = options;
-
-	return new Promise((resolve, reject) => {
-		const handleError = (error) => {
-			if (error.code === 'EADDRINUSE') {
-				console.log(`port ${port} already in use, retrying...`);
-				server.listen(0, host);
-			}
-			else {
-				server.removeListener('error', handleError);
-				reject(error);
-			}
-		};
-
-		server.on('error', handleError);
-
-		server.listen(port, host, () => {
-			server.removeListener('error', handleError);
-
-			const { address: host, port } = server.address();
-			resolve({ host, port });
-		});
-	});
-}
-
-function printServerInfo ({ host, port }) {
-	if (host === '127.0.0.1') {
-		console.info(`> local: http://${host}:${port}`);
-		console.info(`> network: not exposed`);
-	}
-	else {
-		const addresses = Object.values(os.networkInterfaces())
-			.flatMap((intrf) => intrf ?? [])
-			.filter((address) => address.family === 'IPv4');
-
-		for (const address of addresses) {
-			const type = address.internal ? 'local' : 'network';
-			const hostname = address.internal ? 'localhost' : address.address;
-			console.info(`> ${type}: http://${hostname}:${port}`);
-		}
-	}
-
-	console.log('');
-}
+printServerInfo({ host: internal.host, port: internal.port });
