26286d825eee5157936b81ec53891f975f5925a5
intrnl <intrnl28+git@gmail.com>
2023-04-06 07:06:46 +0700

    chore: add nojekyll

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,7 +8,9 @@ fi
 
 GIT_COMMIT=$(git rev-parse HEAD)
 
-rsync -aHAX --delete --exclude=.git dist/ deploy/
+rsync -aHAX --delete --exclude=.git --exclude=.nojekyll dist/ deploy/
+touch deploy/.nojekyll
+
 pnpm exec minify-html deploy/index.html --output deploy/index.html
 
 git -C deploy/ add .
